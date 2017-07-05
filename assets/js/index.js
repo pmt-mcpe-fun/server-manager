2e7786109b79ed455d08340ebe6f86d5de2c5e31
Ad5001 <mail@ad5001.eu>
2017-07-05 19:15:41 +0200

    Revert "Adding stuff(best descriptive commit ever)"
    This reverts commit d555969f00af7180ddbc106a3b114631bac823ea.

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,126 +10,122 @@
 const fs = require('fs');
 window.echanger = require(__dirname + "/echange.js");
 
-window.addEventListener("load", function() {
-    const main = require(__dirname + '/main.js');
-    /**
-     * Adding frame changing API
-     */
-    document.getElementById("frame").addEventListener("load", function(event) {
-        if (document.getElementById("frame").contentWindow.location.pathname.indexOf("serverPicker.html") > 0) {
-            document.getElementById("main-logo").childNodes[0].innerHTML = "home";
-            document.getElementById("main-logo").setAttribute("href", "serverPicker.html#");
-            document.getElementById("main-logo").classList.add("unclickable");
-        } else {
-            document.getElementById("main-logo").childNodes[0].innerHTML = "navigate_before";
-            document.getElementById("main-logo").setAttribute("href", "serverPicker.html");
-            document.getElementById("main-logo").classList.remove("unclickable");
-        }
-    });
+window.addEventListener("load", function () {
+	const main = require(__dirname + '/main.js');
+	/**
+	 * Adding frame changing API
+	 */
+	document.getElementById("frame").addEventListener("load", function (event) {
+		if (document.getElementById("frame").contentWindow.location.pathname.indexOf("serverPicker.html") > 0) {
+			document.getElementById("main-logo").childNodes[0].innerHTML = "home";
+			document.getElementById("main-logo").setAttribute("href", "serverPicker.html#");
+			document.getElementById("main-logo").classList.add("unclickable");
+		} else {
+			document.getElementById("main-logo").childNodes[0].innerHTML = "navigate_before";
+			document.getElementById("main-logo").setAttribute("href", "serverPicker.html");
+			document.getElementById("main-logo").classList.remove("unclickable");
+		}
+	});
 
-    // document.getElementById("main-logo").addEventListener("click", function(event){
-    // 	document.getElementById("frame").src = "serverPicker.html";
-    // 	document.getElementById("frame").contentWindow.location = "serverPicker.html";
-    // 	document.getElementById("frame").contentWindow.location.reload();
-    // 	console.log("Reloading frame");
-    // })
+	// document.getElementById("main-logo").addEventListener("click", function(event){
+	// 	document.getElementById("frame").src = "serverPicker.html";
+	// 	document.getElementById("frame").contentWindow.location = "serverPicker.html";
+	// 	document.getElementById("frame").contentWindow.location.reload();
+	// 	console.log("Reloading frame");
+	// })
 
 
-    /**
-     * Making menu
-     */
-    var menu = new mdc.menu.MDCSimpleMenu(document.querySelector('.mdc-simple-menu'));
-    // Add event listener to some button to toggle the menu on and off.
-    document.querySelector('#open_menu')
-        .addEventListener('click', function() {
-            menu.open = !menu.open
-        });
+	/**
+	 * Making menu
+	 */
+	var menu = new mdc.menu.MDCSimpleMenu(document.querySelector('.mdc-simple-menu'));
+	// Add event listener to some button to toggle the menu on and off.
+	document.querySelector('#open_menu')
+		.addEventListener('click', function () {
+			menu.open = !menu.open
+		});
 
-    /**
-     * Add Server Dialog
-     */
-    var addServerOpen = document.getElementById("addServerOpen");
-    var addServerOpen2 = document.getElementById("footerAddServer");
-    var addServerDialog = new mdc.dialog.MDCDialog(document.getElementById("createServerDialog"));
-    var addServerForm = document.getElementById("addServerForm");
-    /**
-     * Opening buttons
-     */
-    addServerOpen.addEventListener('click', function(evt) {
-        addServerDialog.lastFocusedTarget = evt.target;
-        addServerDialog.show();
-    });
-    addServerOpen2.addEventListener('click', function(evt) {
-        addServerDialog.lastFocusedTarget = evt.target;
-        addServerDialog.show();
-    });
-    /**
-     * Closing buttons
-     */
-    addServerDialog.listen('MDCDialog:accept', function() {
-        if (!document.getElementById("serverName").value.match(/^[\w\-._]+$/)) {
-            formError("Please enter a server name only with alphanumerical, dots (.),\nunderscores (_) and hyphens (-) caracters.");
-            return false;
-        }
-        console.log(main.selects[0].value);
-        if (main.selects[0].value <= 0.1) {
-            formError("Please select a valid MCPE version");
-            return false;
-        }
-        resetForm(addServerForm);
-        document.getElementById("serverPort").value = 19132 + fs.readdirSync(main.serverFolder).length;
-        addServerDialog.close();
-    })
-    addServerDialog.listen('MDCDialog:cancel', function() {})
-    document.getElementById("serverPort").value = 19132 + fs.readdirSync(main.serverFolder).length;
+	/**
+	 * Add Server Dialog
+	 */
+	var addServerOpen = document.getElementById("addServerOpen");
+	var addServerOpen2 = document.getElementById("footerAddServer");
+	var addServerDialog = new mdc.dialog.MDCDialog(document.getElementById("createServerDialog"));
+	var addServerForm = document.getElementById("addServerForm");
+	/**
+	 * Opening buttons
+	 */
+	addServerOpen.addEventListener('click', function (evt) {
+		addServerDialog.lastFocusedTarget = evt.target;
+		addServerDialog.show();
+	});
+	addServerOpen2.addEventListener('click', function (evt) {
+		addServerDialog.lastFocusedTarget = evt.target;
+		addServerDialog.show();
+	});
+	/**
+	 * Closing buttons
+	 */
+	addServerDialog.listen('MDCDialog:accept', function () {
+		if(!document.getElementById("serverName").value.match(/^[\w\-._]+$/)) {
+			formError("Please enter a server name only with alphanumerical, dots (.), underscores (_) and hyphens (-) caracters.");
+			return false;
+		}
+		console.log(main.selects[0].value);
+		if(main.selects[0].value <= 0.1) {
+			formError("Please select a valid MCPE version");
+			return false;
+		}
+		resetForm(addServerForm);
+		document.getElementById("serverPort").value = 19132 + fs.readdirSync(main.serverFolder).length;
+		addServerDialog.close();
+	})
+	addServerDialog.listen('MDCDialog:cancel', function () {
+	})
+	document.getElementById("serverPort").value = 19132 + fs.readdirSync(main.serverFolder).length;
 
-    /**
-     * Clears an HTML form
-     * 
-     * @param {HTMLElement} form 
-     */
-    function resetForm(form) {
-        // clearing inputs
-        var inputs = form.getElementsByTagName('input');
-        for (var i = 0; i < inputs.length; i++) {
-            switch (inputs[i].type) {
-                // case 'hidden':
-                case 'text':
-                    inputs[i].value = '';
-                    break;
-                case 'radio':
-                case 'checkbox':
-                    inputs[i].checked = false;
-            }
-        }
+	/**
+	 * Clears an HTML form
+	 * 
+	 * @param {HTMLElement} form 
+	 */
+	function resetForm(form) {
+		// clearing inputs
+		var inputs = form.getElementsByTagName('input');
+		for (var i = 0; i < inputs.length; i++) {
+			switch (inputs[i].type) {
+				// case 'hidden':
+				case 'text':
+					inputs[i].value = '';
+					break;
+				case 'radio':
+				case 'checkbox':
+					inputs[i].checked = false;
+			}
+		}
 
-        // clearing selects
-        var selects = form.getElementsByTagName('select');
-        for (var i = 0; i < selects.length; i++)
-            selects[i].selectedIndex = 0;
+		// clearing selects
+		var selects = form.getElementsByTagName('select');
+		for (var i = 0; i < selects.length; i++)
+			selects[i].selectedIndex = 0;
 
-        // clearing textarea
-        var text = form.getElementsByTagName('textarea');
-        for (var i = 0; i < text.length; i++)
-            text[i].innerHTML = '';
+		// clearing textarea
+		var text = form.getElementsByTagName('textarea');
+		for (var i = 0; i < text.length; i++)
+			text[i].innerHTML = '';
 
-        return false;
-    }
+		return false;
+	}
 
-    /**
-     * Creates a snackbar
-     * 
-     * @param {String} error 
-     */
-    function formError(error) {
-        var snackbar = new mdc.snackbar.MDCSnackbar(document.querySelector('#formError'));
-        snackbar.show({
-            message: error,
-            actionText: "Dismiss",
-            actionHandler: function() {},
-            multiline: error.indexOf("\n") > 0,
-            actionOnBottom: error.indexOf("\n") > 0
-        });
-        addServerDialog.show();
-    }
+	function formError(error){
+		var snackbar = new mdc.snackbar.MDCSnackbar(document.querySelector('#formError'));
+		snackbar.show({
+			message: error,
+			actionText: "Dismiss",
+			actionHandler: function(){},
+			multiline: true,
+			actionOnBottom: true
+		});
+		addServerDialog.show();
+	}
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,8 @@
  * @copyright (C) Ad5001 2017
  */
 
-if (top) {
-    require = top.window.require;
+if(top) {
+	require = top.window.require;
 }
 const path = require('path');
 const fs = require('fs');
@@ -23,71 +23,23 @@ exports.selects = [];
 // Defining servers folder location
 exports.appFolder = path.join(os.homedir(), '.pocketmine');
 exports.serverFolder = path.join(exports.appFolder, "servers");
-exports.phpFolder = path.join(exports.appFolder, "bin");
 try {
-    fs.accessSync(exports.serverFolder);
+	fs.accessSync(exports.serverFolder);
 } catch (e) { // No .pocketmine folder
-    fs.mkdirSync(exports.appFolder);
-    fs.mkdirSync(exports.serverFolder);
-    fs.mkdirSync(exports.phpFolder);
-    // TODO: Download PHP binaries for correct OS.
+	fs.mkdirSync(exports.appFolder);
+	fs.mkdirSync(exports.serverFolder);
 }
 
-/**
- * Downloads and creates a pocketmine server.
- * 
- * @param {String} name
- * @param {Int} port
- * @param {Float} version
- */
-exports.createPMServer = function(name, port, version) {
-    // Create servers pathes:
-    var serverPath = path.join(exports.serverFolder, name);
-    fs.mkdir(serverPath, function(err) {
-        if (!err) {
-            try {
-                fs.mkdirSync(path.join(serverPath, "plugins"));
-                fs.mkdirSync(path.join(serverPath, "players"));
-                fs.mkdirSync(path.join(serverPath, "worlds"));
-                fs.mkdirSync(path.join(serverPath, "resource_packs"));
-            } catch (e) {
-                snackbar("Could not create server's folders.\nDo you have perms on your home folder?");
-                console.error(e.message);
-            }
-        } else {
-            snackbar("Could not create server's folder.\nAre you sure a server with that name doesn't exists already?");
-            console.error(err.message);
-        }
-    });
-}
+window.addEventListener("load", function (event) {
 
-window.addEventListener("load", function(event) {
-
-    // Making inputs working
-    document.querySelectorAll('.mdc-textfield').forEach(function(elem) {
-        exports.inputs[elem.id] = new mdc.textfield.MDCTextfield(elem, /* foundation */ undefined, (el) => {
-            // do something with el...
-            return new mdc.ripple.MDCRipple(el);
-        });
-    });
-    document.querySelectorAll('.mdc-select').forEach(function(elem) {
-        exports.selects.push(new mdc.select.MDCSelect(elem));
-    });
+	// Making inputs working
+	document.querySelectorAll('.mdc-textfield').forEach(function(elem){
+		exports.inputs[elem.id] = new mdc.textfield.MDCTextfield(elem, /* foundation */ undefined, (el) => {
+		  // do something with el...
+		  return new mdc.ripple.MDCRipple(el);
+		});
+	});
+	document.querySelectorAll('.mdc-select').forEach(function(elem){
+		exports.selects.push(new mdc.select.MDCSelect(elem));
+	});
 });
-
-/**
- * Creates a snackbar
- * 
- * @param {String} error 
- */
-function snackbar(error) {
-    var snackbar = new mdc.snackbar.MDCSnackbar(document.querySelector('#formError'));
-    snackbar.show({
-        message: error,
-        actionText: "Dismiss",
-        actionHandler: function() {},
-        multiline: error.indexOf("\n") > 0,
-        actionOnBottom: error.indexOf("\n") > 0
-    });
-    addServerDialog.show();
-}
