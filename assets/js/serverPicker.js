d555969f00af7180ddbc106a3b114631bac823ea
Ad5001 <mail@ad5001.eu>
2017-07-05 19:14:47 +0200

    Adding stuff(best descriptive commit ever)

--- assets/js/serverPicker.js
+++ assets/js/serverPicker.js
@@ -15,51 +15,51 @@ const properties = require("./js/properties.js");
 const main = require('./js/main.js');
 
 // Defining custom left click element
-document.body.addEventListener("contextmenu", function (event) {
-	var menu = new mdc.menu.MDCSimpleMenu(document.querySelector('.mdc-simple-menu'));
-	console.log(document.getElementById("leftClick"));
-	document.getElementById("leftClick").style.left = event.clientX + 'px';
-	document.getElementById("leftClick").style.top = event.clientY + 'px';
-	// Showing element
-	menu.open = !menu.open;
+document.body.addEventListener("contextmenu", function(event) {
+    var menu = new mdc.menu.MDCSimpleMenu(document.querySelector('.mdc-simple-menu'));
+    console.log(document.getElementById("leftClick"));
+    document.getElementById("leftClick").style.left = event.clientX + 'px';
+    document.getElementById("leftClick").style.top = event.clientY + 'px';
+    // Showing element
+    menu.open = !menu.open;
 });
 
 // Adding add server button
-document.getElementById("addServerOpen").addEventListener('click', function (evt) {
-	var addServerDialog = new mdc.dialog.MDCDialog(top.document.getElementById("createServerDialog"));
-	addServerDialog.lastFocusedTarget = evt.target;
-	addServerDialog.show();
+document.getElementById("addServerOpen").addEventListener('click', function(evt) {
+    var addServerDialog = new mdc.dialog.MDCDialog(top.document.getElementById("createServerDialog"));
+    addServerDialog.lastFocusedTarget = evt.target;
+    addServerDialog.show();
 });
 
 var servers = fs.readdirSync(main.serverFolder);
 if (servers.length > 0) {
-	servers.forEach(function (folder) {
-		try {
-			fs.accessSync(path.join(main.serverFolder, folder, "server.properties"));
-			var running;
-			try{
-				fs.accessSync(path.join(main.serverFolder, folder, "pms.json"));
-				running = true;
-			} catch(e) {
-				running = false;
-			}
-			var serverInfos = properties.parseProperties(fs.readFileSync(path.join(main.serverFolder, folder, "server.properties")).toString());
-			var list = document.getElementById("serverPicker");
-			if (typeof list == "object") {
-				list.innerHTML += '<a href="serverInfos.html#' + folder + '" class="mdc-list-item" data-mdc-auto-init="MDCRipple"> \
-    		<i class="material-icons mdc-list-item__start-detail" style="color: ' + (running ? "play_arrow" : "stop" ) + ';"> \
-      			' + (running ? "play_arrow" : "stop" ) + ' \
+    servers.forEach(function(folder) {
+        try {
+            fs.accessSync(path.join(main.serverFolder, folder, "server.properties"));
+            var running;
+            try {
+                fs.accessSync(path.join(main.serverFolder, folder, "pms.json"));
+                running = true;
+            } catch (e) {
+                running = false;
+            }
+            var serverInfos = properties.parseProperties(fs.readFileSync(path.join(main.serverFolder, folder, "server.properties")).toString());
+            var list = document.getElementById("serverPicker");
+            if (typeof list == "object") {
+                list.innerHTML += '<a href="serverInfos.html#' + folder + '" class="mdc-list-item" data-mdc-auto-init="MDCRipple"> \
+    		<i class="material-icons mdc-list-item__start-detail" style="color: ' + (running ? "play_arrow" : "stop") + ';"> \
+      			' + (running ? "play_arrow" : "stop") + ' \
     		</i> \
     		' + folder + ' \
 			<span class="mdc-list-item__text__secondary">&nbsp;-&nbsp;' + serverInfos["motd"] + '</span> \
 			<i class="material-icons mdc-list-item__start-detail mdc-list-item__end-detail">navigate_next</i> \
   			</a>';
-			}
-		} catch (e) {
-			console.error(e);
-		}
-	}, this);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }, this);
 } else {
-	document.getElementById("page-content").innerHTML += "<h2 style='padding-left: 50px;'>No server created for the moment.</h2> \
+    document.body.innerHTML += "<h2 style='padding-left: 50px;' class='mdc-typography--headline'>No server created for the moment.</h2> \
 	<button style='padding-left: 50px;' class='mdl-button mdl-js-button mdl-button--raised' onclick='document.getElementById(\"createServerDialog\").showModal();'>Create one</button>"
 }
