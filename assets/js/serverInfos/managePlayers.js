8c0dcd4cc47076f4597b7b22484b092c97d33d14
Ad5001 <Ad5001yt@gmail.com>
2017-07-20 16:08:05 +0100

    Starting players managing

---
+++ assets/js/serverInfos/managePlayers.js
@@ -0,0 +1,12 @@
+/**
+ * managePlayers.js - JS File for players managing
+ * 
+ * @author Ad5001
+ * @version 1.0.0
+ * @license CC-BY-NC-SA-4.0
+ * @copyright (C) Ad5001 2017
+ * @package PocketMine Server Manager
+ */
+
+
+ 
