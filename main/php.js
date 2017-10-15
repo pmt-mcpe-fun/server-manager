8f563e587afd3125adfd5784a8a51d3a983c9d3b
Ad5001 <mail@ad5001.eu>
2017-10-15 13:49:55 +0200

    1.1 Version for 1.2 supporting.

--- main/php.js
+++ main/php.js
@@ -1,6 +1,6 @@
 /**
  * php.js - Every php related features
- * 
+ *
  * @author Ad5001
  * @version 1.0.0
  * @license NTOSL (Custom) - View LICENSE.md in the root of the project
@@ -11,15 +11,14 @@
 const os = require('os');
 const path = require('path');
 const fs = require('fs');
-const http = require('https');
-const tarGz = require('node-targz');
+const http = require("https");
+const exec = require('child_process').spawn;
 const { URL } = require("url");
 const properties = require("./lib/properties.js");
-const PHP_VERSION = "7.0.3";
 /**
  * Sets main.js app exports
- * 
- * @param {any} app 
+ *
+ * @param {any} app
  */
 function setApp(app) {
     exports.app = app;
@@ -28,7 +27,7 @@ exports.setApp = setApp;
 
 /**
  * Defines php.
- * 
+ *
  * @param {Function} cb
  */
 function define(cb) {
@@ -51,115 +50,90 @@ exports.define = define;
 
 /**
  * Downloads php.
- * 
+ *
  * @param {Function} cb
  */
 function downloadPHP(cb) {
-    var arch;
-    switch (os.arch()) {
-        case "x64":
-        case "amd64":
-        case "arm64":
-            arch = "x86-64";
-            break;
-        default:
-            arch = "x86";
-            break;
-    }
-    var osName;
-    switch (os.platform()) {
-        case "win32":
-            osName = "Windows";
-            if (arch == "x86-64") arch = "x64";
-            break;
-        case "darwin":
-            osName = "MacOS";
-            break;
-        default:
-            osName = "Linux";
-            break;
-    }
-    snackbar("Downloading PHP v" + PHP_VERSION + "...");
-    exports.download('https://bintray.com/pocketmine/PocketMine/download_file?file_path=PHP_' + PHP_VERSION + '_' + arch + '_' + osName + '.tar.gz',
-        path.join(exports.app.appFolder, "php.tar.gz"),
-        function(err) {
-            if (err) {
-                snackbar("An internet connection is required to download PHP. You may not be able to use your servers until then.");
-                fs.unlink(exports.app.phpFolder);
-                console.error(err);
-            }
-            snackbar("Extracting PHP...");
-            tarGz.decompress({
-                source: path.join(exports.app.appFolder, "php.tar.gz"),
-                destination: exports.app.phpFolder
-            }, function(err) {
-                if (!err) {
-                    // Now we replace the "/PocketMine/" which is the default ~/.pocketmine/php AKA exports.app.phpFolder
-                    walk(exports.app.phpFolder);
-                    try { // Windows
-                        fs.accessSync(path.join(exports.app.phpFolder, "bin", "php")); // Windows
-                        exports.phpExecutable = path.join(exports.app.phpFolder, "bin", "php", "php.exe");
-                        exports.phpIni = path.join(exports.app.phpFolder, "bin", "php", "php.ini");
-                        exports.phpPath = path.join(exports.app.phpFolder, "bin", "php");
-                    } catch (e) { // Linux & MacOS & Android
-                        exports.phpExecutable = path.join(exports.app.phpFolder, "bin", "php7", "bin", "php");
-                        exports.phpIni = path.join(exports.app.phpFolder, "bin", "php7", "bin", "php.ini");
-                        exports.phpPath = path.join(exports.app.phpFolder, "bin", "php7");
-                    }
-                    /**
-                     * PHP.ini replacements, due to extensions locations... WHY???
-                     */
-                    fs.readFile(exports.phpIni, function(err, data) {
-                        if (err) {
-                            console.log(err);
-                            snackbar("Could not read php.ini. Are youu sure that you can read files in your home directory?");
-                        } else {
-                            var properties = data.toString().split(os.EOL);
-                            if (os.platform() !== "win32") properties.push("extension_dir=" + path.join(exports.phpPath, "lib"));
-                            properties.forEach(function(line, index) {
-                                var lineData = line.split("=");
-                                if (lineData[0] == "zend_extension") {
-                                    switch (lineData[1]) {
-                                        case "opcache.so":
-                                            fs.renameSync(path.join(exports.phpPath, "lib", "php", "extensions",
-                                                    fs.readdirSync(path.join(exports.phpPath, "lib", "php", "extensions"))[0], "opcache.so"),
-                                                path.join(exports.phpPath, "lib", "opcache.so"));
-                                            break;
-                                    }
-                                } else if (lineData[0] == "extension") {
-                                    switch (lineData[1]) {
-                                        case "php_pthreads.dll":
-                                            properties[index] = "extension=php_pthreadsVC2.dll";
-                                            break;
-                                    }
-                                }
-                            });
-                            fs.writeFile(exports.phpIni, properties.join(os.EOL), function(err) {
-                                if (err) {
-                                    console.log(err);
-                                    snackbar("Could not read php.ini. Are youu sure that you can read files in your home directory?");
-                                } else {
-                                    fs.unlink(path.join(exports.app.appFolder, "php.tar.gz"));
-                                    snackbar("Successfully downloaded PHP " + PHP_VERSION + ".");
-                                    cb.apply(exports.app);
-                                }
-                            })
-                        }
+    snackbar("Downloading PHP...");
+    fs.mkdir(exports.app.phpFolder, function(){
+        var osName;
+        switch (os.platform()) {
+            // Windows binaries
+            case "win32":
+                exports.download("https://psm.mcpe.fun/download/PHP/windows-binaries.ps1",
+                    path.join(exports.app.phpFolder, "compile.ps1"), () => {
+                        snackbar("Compiling PHP...");
+                        var comp = exec("PowerShell.exe",
+                            ["-File", path.join(exports.app.phpFolder, "compile.ps1")],
+                            {cwd:exports.app.phpFolder}
+                        );
+                        comp.stdout.on('data', exports.logCompile);
+                        comp.stderr.on('data', exports.logCompile);
+                        comp.on("close",(code) => {
+                            if(code !== 0){
+                                snackbar("Could not compile PHP: " + code + ". For more informations, look at " + path.join(exports.app.phpFolder, "install.log"));
+                            } else {
+                                snackbar("Finished compiling PHP!");
+                                exports.phpExecutable = path.join(exports.app.phpFolder, "bin", "php", "php.exe");
+                                cb.apply(exports.app);
+                            }
+                        })
                     })
-                } else {
-                    console.log(err);
-                    snackbar("Could not extract PHP " + PHP_VERSION);
-                }
-            });
-        });
+                break;
+            // Linux and MacOS binaries
+            default:
+                exports.download("https://psm.mcpe.fun/download/PHP/compile.sh",
+                    path.join(exports.app.phpFolder, "compile.sh"), () => {
+                        snackbar("Compiling PHP...");
+                        var comp = exec("bash",
+                            [path.join(exports.app.phpFolder, "compile.sh")],
+                            {cwd:exports.app.phpFolder}
+                        );
+                        comp.stdout.on('data', exports.logCompile);
+                        comp.stderr.on('data', exports.logCompile);
+                        comp.on("close",(code) => {
+                            if(code !== 0){
+                                snackbar("Could not compile PHP: " + code + ". For more informations, look at " + path.join(exports.app.phpFolder, "install.log"));
+                            } else {
+                                snackbar("Finished compiling PHP!");
+                                exports.phpExecutable = path.join(exports.app.phpFolder, "bin", "php7", "bin", "php");
+                                cb.apply(exports.app);
+                            }
+                        })
+                    }
+                )
+                break;
+        }
+    });
+}
+
+
+/**
+ * Logs compileing data
+ *
+ * @param {Buffer} data
+ */
+var currentCmp = "";
+exports.logCompile = (data) => {
+    console.log(data.toString());
+    var cmp = /^(\[\w+\])/.exec(data.toString());
+    if(cmp && cmp[1]) {
+        currentCmp = cmp[1];
+        snackbar("Compiling PHP...<br>" + data.toString());
+    } else {
+        snackbar("Compiling PHP...<br>" + currentCmp + data.toString());
+    }
+    if(data.toString().includes("[ERROR]")) {
+        snackbar("Could not compile PHP: <br>" + data.toString() + ".<br> For more informations, look at " + path.join(exports.app.phpFolder, "install.log"));
+    }
 }
 
 /**
  * Downloads a file
- * 
+ *
  * @param {String} urlStr
- * @param {String} dest 
- * @param {Function} cb 
+ * @param {String} dest
+ * @param {Function} cb
  */
 exports.download = function(urlStr, dest, cb) {
     var res;
@@ -186,36 +160,10 @@ exports.download = function(urlStr, dest, cb) {
 
 /**
  * Submits an message (during loading)
- * 
- * @param {String} error 
+ *
+ * @param {String} error
  */
 function snackbar(error) {
     if (exports.app && exports.app.mainWindow) exports.app.mainWindow.webContents.executeJavaScript(`if(document.getElementById('currentThing')) document.getElementById('currentThing').innerHTML = "${error}"`);
 }
 exports.snackbar = snackbar;
-
-/**
- * Replaces every /PocketMine to data dir.
- * 
- * @param {String} dir 
- */
-var walk = function(dir) {
-    var list = fs.readdirSync(dir)
-    list.forEach(function(oldfile) {
-        file = path.join(dir, oldfile);
-        var stat = fs.statSync(file)
-        if (stat && stat.isDirectory()) {
-            walk(file);
-        } else {
-            var contents = fs.readFileSync(file).toString();
-            var newContents = contents.replace(/\/PocketMine/g, exports.app.phpFolder);
-            if (contents !== newContents &&
-                oldfile !== "php" &&
-                oldfile.split(".")[oldfile.split(".").length - 1] !== "exe" &&
-                oldfile.split(".")[oldfile.split(".").length - 1] !== "dll" &&
-                oldfile.split(".")[oldfile.split(".").length - 1] !== "so"
-                /*Preventing bin to be overwritten so broken*/
-            ) fs.writeFileSync(file, newContents);
-        }
-    })
-}
